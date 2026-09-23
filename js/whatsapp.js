42e74565d1bae98d95186aae73867b8c1edff260
Bernard Victor Kirinya <123264661+sudoApT-getVickie@users.noreply.github.com>
2026-09-23 15:36:24 +0300

    Change WhatsApp phone number to international format
    Updated the phone number for Eleazar Barbershop from local to international format.

--- js/whatsapp.js
+++ js/whatsapp.js
@@ -6,7 +6,9 @@
  * @param {string} preferredTime - The user's selected time block.
  */
 function generateWhatsAppLink(serviceName, servicePrice, preferredDay, preferredTime) {
-    const phoneNumber = "254710247959"; 
+    // Updated to the new Eleazar Barbershop number
+    // Converted local 0716426596 to international 254716426596
+    const phoneNumber = "254716426596"; 
     
     const message = `Hello 👋, I would like to book an appointment.\n\nService: ${serviceName} (${servicePrice})\nPreferred Day: ${preferredDay}\nPreferred Time: ${preferredTime}\n\nDo you have a slot open?`;
     
